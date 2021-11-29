95bc47599061ca624ed1c408eeb0426ab3dde779
vagi8 <mankalavageeshan1@gmail.com>
2021-11-29 01:53:33 -0700

    bug fixes

--- deepweed_agave.py
+++ deepweed_agave.py
@@ -75,7 +75,7 @@ def generate_data(combined_data,image_mode,model_name):
             unique_label.append(species_name)
         unique_y.append(label)
     
-    X = image_reshape(X,image_mode)
+    X = image_reshape(X,image_mode,model_name)
 
     print("Splitting train & test")
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
@@ -90,6 +90,116 @@ def generate_data(combined_data,image_mode,model_name):
     print(f"Y Validation : {len(y_val)}")
     return X_train, X_test, y_train, y_test, X_val, y_val, unique_label, unique_y
 
+def f1_score(y_true, y_pred):
+        true_positives_values = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positive_value = K.sum(K.round(K.clip(y_true, 0, 1)))
+        predicted_positives_value = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives_values / (predicted_positives_value + K.epsilon())
+        recall = true_positives_values / (possible_positive_value + K.epsilon())
+        f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
+        return f1_val
+
+def train_Validation_plot(acc,val_acc,loss,val_loss,auc,val_auc,precision,val_precision,f1,val_f1):
+    fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1,5, figsize= (20,5))
+    fig.suptitle("Visualization of Model")
+    
+    # Figure 1
+    fig,ax = plt.subplots(figsize= (20,5))
+    ax.plot(range(1, len(acc) + 1), acc)
+    ax.plot(range(1, len(val_acc) + 1), val_acc)
+    ax.set_title('Accuracy')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    ax.legend(['training', 'validation'])
+    
+    # Figure 2
+    fig,ax = plt.subplots(figsize= (20,5))
+    ax.plot(range(1, len(loss) + 1), loss)
+    ax.plot(range(1, len(val_loss) + 1), val_loss)
+    ax.set_title('Loss Curve')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend(['training', 'validation'])
+    
+    # Figure 3
+    fig,ax = plt.subplots(figsize= (20,5))
+    ax.plot(range(1, len(auc) + 1), auc)
+    ax.plot(range(1, len(val_auc) + 1), val_auc)
+    ax.set_title('AUC Curve')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('AUC')
+    ax.legend(['training', 'validation'])
+    
+    # Figure 4
+    fig,ax = plt.subplots(figsize= (20,5))
+    ax.plot(range(1, len(precision) + 1), precision)
+    ax.plot(range(1, len(val_precision) + 1), val_precision)
+    ax.set_title('Precision')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Precision')
+    ax.legend(['training', 'validation'])
+    
+    # Figure 5
+    fig,ax = plt.subplots(figsize= (20,5))
+    ax.plot(range(1, len(f1) + 1), f1)
+    ax.plot(range(1, len(val_f1) + 1), val_f1)
+    ax.set_title('F1-score')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('F1 score')
+    ax.legend(['training', 'validation'])
+    
+    plt.show()
+
+def plot_confusion_matrix(cm,
+                        target_names,
+                        title='Confusion matrix',
+                        cmap=None,
+                        normalize=True):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    
+
+    accuracy = np.trace(cm) / np.sum(cm).astype('float')
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(15, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.savefig(confusion_matrix_path,dpi=300, bbox_inches = "tight")
+
+def classification_report_txt(report):
+    with open(classification_report_path, 'w') as f:
+        f.write(report)
+        
 def run_model(model_name):
     print("model_name - ", model_name)
     model_weights_path,model_json_path,confusion_matrix_path,classification_report_path=define_paths(model_name)
@@ -120,14 +230,7 @@ def run_model(model_name):
     model.add(Dense(9,activation='sigmoid'))
 
     # defining f1_scroe mteric algorithm
-    def f1_score(y_true, y_pred):
-        true_positives_values = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-        possible_positive_value = K.sum(K.round(K.clip(y_true, 0, 1)))
-        predicted_positives_value = K.sum(K.round(K.clip(y_pred, 0, 1)))
-        precision = true_positives_values / (predicted_positives_value + K.epsilon())
-        recall = true_positives_values / (possible_positive_value + K.epsilon())
-        f1_val = 2*(precision*recall)/(precision+recall+K.epsilon())
-        return f1_val
+    
 
     # Model metrics options
     METRICS = [BinaryAccuracy(name='accuracy'),Precision(name='precision'),Recall(name='recall'),AUC(name='auc'),f1_score]
@@ -158,56 +261,6 @@ def run_model(model_name):
     #%% PLOTTING RESULTS (Train vs Validation FOLDER 1)
 
     # Function to create model training stats
-    def train_Validation_plot(acc,val_acc,loss,val_loss,auc,val_auc,precision,val_precision,f1,val_f1):
-        fig, (ax1, ax2,ax3,ax4,ax5) = plt.subplots(1,5, figsize= (20,5))
-        fig.suptitle("Visualization of Model")
-        
-        # Figure 1
-        fig,ax = plt.subplots(figsize= (20,5))
-        ax.plot(range(1, len(acc) + 1), acc)
-        ax.plot(range(1, len(val_acc) + 1), val_acc)
-        ax.set_title('Accuracy')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel('Accuracy')
-        ax.legend(['training', 'validation'])
-        
-        # Figure 2
-        fig,ax = plt.subplots(figsize= (20,5))
-        ax.plot(range(1, len(loss) + 1), loss)
-        ax.plot(range(1, len(val_loss) + 1), val_loss)
-        ax.set_title('Loss Curve')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel('Loss')
-        ax.legend(['training', 'validation'])
-        
-        # Figure 3
-        fig,ax = plt.subplots(figsize= (20,5))
-        ax.plot(range(1, len(auc) + 1), auc)
-        ax.plot(range(1, len(val_auc) + 1), val_auc)
-        ax.set_title('AUC Curve')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel('AUC')
-        ax.legend(['training', 'validation'])
-        
-        # Figure 4
-        fig,ax = plt.subplots(figsize= (20,5))
-        ax.plot(range(1, len(precision) + 1), precision)
-        ax.plot(range(1, len(val_precision) + 1), val_precision)
-        ax.set_title('Precision')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel('Precision')
-        ax.legend(['training', 'validation'])
-        
-        # Figure 5
-        fig,ax = plt.subplots(figsize= (20,5))
-        ax.plot(range(1, len(f1) + 1), f1)
-        ax.plot(range(1, len(val_f1) + 1), val_f1)
-        ax.set_title('F1-score')
-        ax.set_xlabel('Epochs')
-        ax.set_ylabel('F1 score')
-        ax.legend(['training', 'validation'])
-        
-        plt.show()
 
     # Getting model stats Graph
     train_Validation_plot(history.history['accuracy'],history.history['val_accuracy'],history.history['loss'],history.history['val_loss'],history.history['auc'],history.history['val_auc'],history.history['precision'],history.history['val_precision'],history.history['f1_score'],history.history['val_f1_score'])
@@ -224,61 +277,10 @@ def run_model(model_name):
         y_pred.append(temp)
         i+=1
 
-
-    def plot_confusion_matrix(cm,
-                            target_names,
-                            title='Confusion matrix',
-                            cmap=None,
-                            normalize=True):
-        import matplotlib.pyplot as plt
-        import numpy as np
-        
-
-        accuracy = np.trace(cm) / np.sum(cm).astype('float')
-        misclass = 1 - accuracy
-
-        if cmap is None:
-            cmap = plt.get_cmap('Blues')
-
-        plt.figure(figsize=(15, 10))
-        plt.imshow(cm, interpolation='nearest', cmap=cmap)
-        plt.title(title)
-        plt.colorbar()
-
-        if target_names is not None:
-            tick_marks = np.arange(len(target_names))
-            plt.xticks(tick_marks, target_names, rotation=45)
-            plt.yticks(tick_marks, target_names)
-
-        if normalize:
-            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
-        thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            if normalize:
-                plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                        horizontalalignment="center",
-                        color="white" if cm[i, j] > thresh else "black")
-            else:
-                plt.text(j, i, "{:,}".format(cm[i, j]),
-                        horizontalalignment="center",
-                        color="white" if cm[i, j] > thresh else "black")
-
-
-        plt.tight_layout()
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-        plt.savefig(confusion_matrix_path,dpi=300, bbox_inches = "tight")
-
     print(type(y_val))
     print(len(y_pred))
 
     plot_confusion_matrix(cm=confusion_matrix(y_true=y_val, y_pred=y_pred), target_names=unique_label, normalize=False)
-
-    def classification_report_txt(report):
-        with open(classification_report_path, 'w') as f:
-            f.write(report)
 
     report = classification_report(y_val, y_pred, target_names=unique_label)
     classification_report_txt(report)
