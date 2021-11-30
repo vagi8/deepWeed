4e61ceda043e054dadb8be3f38ef5bf61e5ecca0
vagi8 <mankalavageeshan1@gmail.com>
2021-11-30 15:41:25 -0700

    created 75*75 pkl

--- load_data.py
+++ load_data.py
@@ -13,7 +13,7 @@ def read_image_array_data(data, df):
     for image in os.listdir(data):
         if image.endswith('.jpg'):
             img = cv2.imread(data+image ,cv2.IMREAD_COLOR)
-            # img = cv2.resize(img, (64,64), interpolation=cv2.INTER_NEAREST)
+            img = cv2.resize(img, (75,75), interpolation=cv2.INTER_NEAREST)
             image_arrays.append([img, df.loc[df['Filename']==image,'Label'].item(),df.loc[df['Filename']==image,'Species'].item()])
         else:
             print('not jpg - ', data+image)
@@ -31,7 +31,7 @@ def load_data_to_pickle(data, label,pickle_name):
 if __name__ == '__main__':
     label = '../deepWeed_dataset/weed_labels.csv'
     data = '../deepWeed_dataset/weed/'
-    pickle_name = 'weed_train_array_full.pkl'
+    pickle_name = 'weed_train_array_75.pkl'
     load_data_to_pickle(data,label,pickle_name)
 
 
